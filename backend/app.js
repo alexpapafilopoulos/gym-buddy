0a170109912186b04bd7166e631c596c0a697576
el20155 <127700111+alexpapafilopoulos@users.noreply.github.com>
2024-10-01 18:48:37 +0300

    Initialized home page with head and footer

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const routes =require('./routes/routes');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json({limit:'200mb'}));
+app.use(bodyParser.urlencoded({ limit:'200mb',extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 const db = require("./config/database");
 const users = require("./models/user");
 const exercises = require("./models/exercise");
 const workouts = require("./models/workout");
-
 const {DataTypes} = require("sequelize");
 const User = users(db,DataTypes);
 const Exercise= exercises(db, DataTypes);
 const Workout= workouts(db, DataTypes);
+
 
 const healthTest = async () => {
     console.log("Testing the database connection..");
@@ -29,6 +36,9 @@ const healthTest = async () => {
   */
 healthTest();
 
+app.get('/', (req, res) => {
+   res.render('index', {pageTitle: "Home"});
+ });
  db.sync().then((req) => {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
